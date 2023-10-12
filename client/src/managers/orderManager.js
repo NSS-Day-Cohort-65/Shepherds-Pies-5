effde9dc389536d6ba5967974a575016dfc3bb74
Kenny Clayton <kenny.d.clayton@gmail.com>
2023-10-12 12:20:35 -0400

    feature 6 delete function working

--- client/src/managers/orderManager.js
+++ client/src/managers/orderManager.js
@@ -1,11 +1,17 @@
 const apiUrl = "/api/order";
 
-export const getOrders = () =>
-{
-    return fetch(apiUrl).then((res) => res.json())
+export const getOrders = () => {
+  return fetch(apiUrl).then((res) => res.json());
 };
 
-export const getOrderById = (id) => 
-{
-    return fetch(`${apiUrl}/${id}`).then((res) => res.json());
-}
+export const getOrderById = (id) => {
+  return fetch(`${apiUrl}/${id}`).then((res) => res.json());
+};
+
+//^ DELETE - this function deletes an order
+export const deleteThisOrder = (id) => {
+  return fetch(`${apiUrl}/${id}`, {
+    method: "DELETE",
+  });
+};
+ 
